580aec835a90dba44c1565af03f00ad6c76b2b6b
Evgenij Beloded <belodedevgenij@gmail.com>
2021-11-04 14:29:39 +0700

    Use cjs for commonjs module

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 // vite.config.js
 
 import { defineConfig } from 'vite'
-
+const name = 'object-call-utils'
 export default defineConfig({
   build: {
     lib: {
       entry: new URL('src/lib/index.ts', import.meta.url).pathname,
-
-      name: 'object-call-utils',
+      formats: ['cjs', 'es'],
+      name,
+      fileName: (format) =>
+        ({
+          cjs: 'lib.cjs',
+          es: 'lib.js',
+        }[format]),
     },
   },
 })
